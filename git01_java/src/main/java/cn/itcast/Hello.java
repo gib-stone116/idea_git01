7c1120c80d1112e6eef4b1213d3f11ef63397e96
stone <stone@163.com>
2019-06-24 15:36:44 +0800

    提交到本地仓库

---
+++ git01_java/src/main/java/cn/itcast/Hello.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * describe:
+ *
+ * @author stone
+ * @date 2019/06/24
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
